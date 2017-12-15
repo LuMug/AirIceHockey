e3558db90889824d9f83b2d8652f96dbe8481d06
alessiax8 <31542268+alessiax8@users.noreply.github.com>
2017-12-15 16:17:22 +0100

    Add files via upload

---
+++ Programmi Arduino - Motori e WiFi/punti.ino
@@ -0,0 +1,61 @@
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_7segment matrix = Adafruit_7segment();
+const int buttonPin = 2;
+//Scrive il punteggio la prima volta
+int i = 1;
+
+//Punteggio primo giocatore
+int p1 = 0;
+//Aggiunge un punto
+boolean a = false;
+
+void setup() {
+pinMode(buttonPin, INPUT);
+
+#ifndef __AVR_ATtiny85__
+  Serial.begin(9600);
+  Serial.println("7 Segment Backpack Test");
+#endif
+  matrix.begin(0x70);
+
+  //attachInterrupt(digitalPinToInterrupt(buttonPin), punteggio, HIGH);
+}
+
+void loop() {
+  /* 
+   *  writeDigitNum(position, value, point)
+   *  position --> posizione in cui scrivere (0-4, 2 escluso)
+   *  value    --> numero che si vuole scrivere (0-9)
+   *  point    --> se il punto del relativo numero è attivo o meno (true/false)
+   *  
+   *  drawColon(twoPoint)
+   *  twoPoint --> se i due punti sono attivi o meno (true/false)
+   *  
+   *  Pin:
+   *  + 3.3V
+   *  - GND
+   *  D A4
+   *  C A5
+   */
+   //Scrive sul 7segmenti
+   matrix.drawColon(true);
+   matrix.writeDigitNum(0, 0, false);
+   matrix.writeDigitNum(1, 0, false);
+   matrix.writeDigitNum(3, 0, false);
+   matrix.writeDigitNum(4, p1, false);
+
+   matrix.writeDisplay();
+
+   if(digitalRead(buttonPin)){
+      p1++;
+      delay(1000);
+   }
+}
+
+void punteggio(){
+   //p1++;
+}
---
+++ Programmi Arduino - Motori e WiFi/timer.ino
@@ -0,0 +1,71 @@
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_7segment matrix = Adafruit_7segment();
+const int buttonPin = 2;
+const int ledPin = 9;
+
+const int MIN_SEC_MINUTE = 0;
+
+int mIniziale = 1;
+int s1 = MIN_SEC_MINUTE;
+int s2 = MIN_SEC_MINUTE;
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+          
+  #ifndef __AVR_ATtiny85__
+    Serial.begin(9600);
+    Serial.println("7 Segment Backpack Test");
+  #endif
+    matrix.begin(0x70);
+}
+
+void loop() {
+   int m = mIniziale;
+
+   matrix.drawColon(true);
+   matrix.writeDigitNum(0, 0, false);
+   matrix.writeDigitNum(1, m, false);
+   matrix.writeDigitNum(3, 0, false);
+   matrix.writeDigitNum(4, 0, false);
+   matrix.writeDisplay();
+   
+   //Parte al click del bottone
+   if(digitalRead(buttonPin)){
+     int t = 60*m;
+     analogWrite(ledPin, LOW);
+     for(int i = 0; i < t; i++){
+       //Scrive sul 7segmenti
+       matrix.drawColon(true);
+       matrix.writeDigitNum(0, 0, false);
+       matrix.writeDigitNum(1, m, false);
+       matrix.writeDigitNum(3, s1, false);
+       matrix.writeDigitNum(4, s2, false);
+       matrix.writeDisplay();
+    
+       //Conto alla rovescia
+       if(s1 == MIN_SEC_MINUTE && s2 == MIN_SEC_MINUTE){
+        m--;
+        s1 = 5;
+        s2 = 9;
+       }else if(s2 == MIN_SEC_MINUTE){
+        s1--;
+        s2 = 9;
+       }else{
+        s2--;
+       }
+    
+       delay(1000);
+       if(m == MIN_SEC_MINUTE && 
+         s1 == MIN_SEC_MINUTE && 
+         s2 == MIN_SEC_MINUTE){
+          Serial.println("STOP");
+          analogWrite(ledPin, HIGH);
+       }
+     }
+   }
+}
